e53fddcbdeec135aea45acb8b36e37e8425db7cd
Topu Roy <topuroy143333@gmail.com>
2024-12-08 22:15:14 +0600

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import "./src/env.js";
+
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
